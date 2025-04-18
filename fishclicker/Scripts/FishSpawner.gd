5a9f62bf67c25a767dff1edf65fa1e09979692b5
Mathew Tran <mathew.tran.dev@gmail.com>
2025-04-18 08:24:45 -0700

    finish none bait pool, player now has chance to pickup garbage

--- fishclicker/Scripts/FishSpawner.gd
+++ fishclicker/Scripts/FishSpawner.gd
@@ -20,10 +20,18 @@ func _on_timer_timeout():
 func SpawnFish():
 	var Spawns = get_tree().get_nodes_in_group("SpawnMarker")
 	Spawns.shuffle()
+	
+
 	var newSpawn = Spawns[0].global_position
 	var fishInstance = load("res://Prefabs/Fish.tscn").instantiate() as Fish
 	fishInstance.global_position = newSpawn
-	fishInstance.Setup(Finder.GetGameManager().CurrentBait.GetRandomFish())
+	
+	var result = randi() % 100
+	if result <= 10:
+		var garbage = load("res://Resources/Levels/Baits/BAIT_GARBAGE.tres").GetRandomFish()
+		fishInstance.Setup(garbage)
+	else:
+		fishInstance.Setup(Finder.GetGameManager().CurrentBait.GetRandomFish())
 	add_child(fishInstance)
 	fishInstance.Dead.connect(OnFishDead)
 	fishInstance.Caught.connect(OnFishCaught)
